950a5a119ec75070707d7a6c061ffd76df147425
Thedoc Scripts <82754205+thedocscripts@users.noreply.github.com>
2022-05-21 13:52:52 +0200

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+Config.time = 65 -- 4 Hours in seconds
+
+--msg Headers --------
+Config.MessageHeader = "Grua"
+
+--Server Notifications
+Config.oneminute = "La grua va a pasar en 1 minuto"
+Config.ThirtySeconds = "La grua va a pasar en 30 segundos"
+Config.FifteenSeconds = "La grua va a pasar en 15 segundos"
+Config.TenSeconds = "La grua va a pasar en 10 segundos"
+Config.DeletedAllVehs = "La grua ha borrado correctamente todos los vehiculos"
+
+
+
+--Client Notifications
+Config.AddedToWhitelist = "Este vehiculo a sido añadido a la whitelist"
+Config.AlreadyInWhitelist = "Este vehiculo ya esta asignado para no borrarse"
