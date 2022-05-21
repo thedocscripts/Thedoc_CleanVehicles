13e836d28a2e96e8fd8e8084d23833d26b8ac5e2
Thedoc Scripts <82754205+thedocscripts@users.noreply.github.com>
2022-05-21 13:53:22 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,10 +1,8 @@
 local QBCore = exports['qb-core']:GetCoreObject()
-local timer = 14400
+local timer = Config.time
 local whitelist = {}
 
--- RegisterCommand('testss', function()
---     TriggerClientEvent('Thedoc_CleanVehicles:client:delveh', -1)
--- end)
+
 
 RegisterNetEvent('Thedoc_CleanVehicles:addtowhitelist')
 AddEventHandler('Thedoc_CleanVehicles:addtowhitelist', function(veh)
@@ -27,17 +25,17 @@ Citizen.CreateThread(function()
     while true do
         timer = timer - 1
         if timer == 60 then
-            TriggerClientEvent('Thedoc_CleanVehicles:client:announce', -1, "La grua va a pasar en 1 minuto")
+            TriggerClientEvent('Thedoc_CleanVehicles:client:announce', -1, Config.oneminute)
         elseif timer == 30 then
-            TriggerClientEvent('Thedoc_CleanVehicles:client:announce', -1, "La grua va a pasar en 30 segundos")
+            TriggerClientEvent('Thedoc_CleanVehicles:client:announce', -1, Config.ThirtySeconds)
         elseif timer == 15 then
-            TriggerClientEvent('Thedoc_CleanVehicles:client:announce', -1, "La grua va a pasar en 15 segundos")
+            TriggerClientEvent('Thedoc_CleanVehicles:client:announce', -1, Config.FifteenSeconds)
         elseif timer == 10 then
-            TriggerClientEvent('Thedoc_CleanVehicles:client:announce', -1, "La grua va a pasar en 10 segundos")
+            TriggerClientEvent('Thedoc_CleanVehicles:client:announce', -1, Config.TenSeconds)
         elseif timer == 0 then
             TriggerClientEvent('Thedoc_CleanVehicles:client:delveh', -1)
-            TriggerClientEvent('Thedoc_CleanVehicles:client:announce', -1, "La grua ha borrado correctamente todos los vehiculos")
-            timer = 14400
+            TriggerClientEvent('Thedoc_CleanVehicles:client:announce', -1, Config.DeletedAllVehs)
+            timer = Config.time
         end
         Wait(1000)
     end
